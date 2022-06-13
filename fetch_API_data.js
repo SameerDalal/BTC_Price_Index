559894d8ab9649ed4322208301a415683e5bfde6
SameerDalal <sameerdalal747@gmail.com>
2022-06-13 18:29:13 -0400

    program is now functional with the addition of chart.js

--- fetch_API_data.js
+++ fetch_API_data.js
@@ -5,22 +5,18 @@ const url = "https://data.messari.io/api/v1/assets/btc/metrics";
 async function getData(){
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data.data.market_data.price_usd);
         return data.data.market_data.price_usd;
+}
+
+
+async function getConstData(){
+    await sleepThread(5000);
+    return getData();
+
 }
 
 
 async function sleepThread(ms){
     return new Promise(resolve => setTimeout(resolve,ms))
-}
-
-
-async function getConstData(){
-    dataInList = [];
-    for (let i = 0; i < 10; i++){
-        dataInList.push(getData());
-        await sleepThread(5000);
-    }
-    return dataInList;
 }
 
